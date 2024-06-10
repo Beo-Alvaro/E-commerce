76affae83bd9c2d5ffefff75ef291739a958d535
Beo Salguero <salguerobeo@gmail.com>
2024-06-10 21:54:16 +0800

    new feature

--- addgames.php
+++ addgames.php
@@ -4,22 +4,52 @@
     }
 ?>
 
+<h3 class="text-center mb-5">ADD GAMES</h3>
 
-<form action="" method="POST" class="mb-2">
+<form action="" method="POST" class="mb-2" enctype="multipart/form-data">
 
-    <h3>ADD GAMES</h3>
+    
 
-    <div class="input w-90 mb-2">
-        <div class="input-group flex-nowrap">
-            <span class="input-group-text" id="addon-wrapping"><i class="fa-solid fa-reciept"></i>
-            </span>
-            <input type="text" class="form-control" placeholder="Name" name="add-game" aria-label="Username" aria-describedby="addon-wrapping">
-        </div>
+    <div class="form-outline w-50 mb-4 mt-4 m-auto">
+            <label for="game_title" class="form-label">Game Title</label>
+            <input type="text" id="game_title" class="form-control" required="required  " placeholder="Enter Game Title" name="add-game" aria-label="gamename" aria-describedby="addon-wrapping" autocomplete="off">
     </div>
 
-    <div class="input w-90 mb-2">
+    <div class="form-outline w-50 mb-4 mt-4 m-auto">
+            <label for="game_desc" class="form-label">Game Description</label>
+            <input type="text" class="form-control" placeholder="Enter Game Description" name="add-desc" aria-label="gamedesc" aria-describedby="addon-wrapping">
+    </div>
+
+    <div class="form-outline w-50 mb-4 mt-4 m-auto">
+            <label for="game_keyword" class="form-label">Game Keyword</label>
+            <input type="text" class="form-control" placeholder="Enter Game Keyword" name="add-keyword" aria-label="gamekey" aria-describedby="addon-wrapping">
+    </div>
+
+    <div class="form-outline w-50 mb-4 mt-4 m-auto">
+            <label for="game_genre" class="form-label">Game Genre</label>
+            <select name="add-genre" id="game_genre" class="form-select">
+                <option value="">Select Genre</option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+            </select>
+    </div>
+
+    <div class="form-outline w-50 mb-4 mt-4 m-auto">
+            <label for="game_image" class="form-label">Upload Image</label>
+            <input type="file" class="form-control" name="add-img" aria-label="gamedesc" aria-describedby="addon-wrapping">
+    </div>
+
+    <div class="form-outline w-50 mb-4 mt-4 m-auto">
+            <label for="game_price" class="form-label">Game Price</label>
+            <input type="text" class="form-control" placeholder="Enter Game Price" name="add-price" aria-label="gamedesc" aria-describedby="addon-wrapping">
+    </div>
+
+
+    <div class="form-outline w-50 mb-4 mt-4 m-auto">
          <div class="input-group flex-nowrap">
-            <input type="submit" class="btn btn-dark p-2 my-3 border-0" value="Submit" name="submit-game" 
+            <input type="submit" class="btn btn-dark px-3 my-3 border-0" value="Submit" name="submit-game" 
             aria-label="Username" aria-describedby="addon-wrapping"> 
         </div>
     </div>
--- connection.php
+++ connection.php
@@ -1,1 +1,11 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$dbname="e-commerce";
 
+$conn = new mysqli($host, $user, $password, $dbname)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+//$con->close();
+?>
