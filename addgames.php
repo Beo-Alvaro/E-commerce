4be9cb1a4870d7f107b3311875faa55c902e7427
coconixx <charlesrull18@gmail.com>
2024-06-10 22:15:14 +0800

    added login connection

--- addgames.php
+++ addgames.php
@@ -1,56 +1,104 @@
 <?php
-    if(isset($_POST['submit-game'])) {
+    if ($_SERVER["REQUEST_METHOD"]=="POST"){
+        require_once('connection.php');
+        $errors = array();
+        if(!empty($_POST['add-game'])){
+            $gameName = mysqli_real_escape_string($conn, trim($_POST['add-game']));
+        }else{
+        $errors[] = "no game input";
+        }
 
+        if(!empty($_POST['add-desc'])){
+            $gameDesc = mysqli_real_escape_string($conn, trim($_POST['add-desc']));
+        }else{
+        $errors[] = "no desc input";
+        }
+
+        if(!empty($_POST['add-price'])){
+            $gamePrice = mysqli_real_escape_string($conn, trim($_POST['add-price']));
+        }else{
+        $errors[] = "no price input";
+        }
+
+        /* if(!empty($_POST['add-picture'])){
+            $gameImg = mysqli_real_escape_string($conn, trim($_POST['add-picture']));
+        }else{
+        $errors[] = "no img input";
+        } */
+        
+        if(!empty($_POST['add-category'])){
+            $gameCategory = mysqli_real_escape_string($conn, trim($_POST['add-category']));
+        }else{
+        $errors[] = "no category chosen";
+        }
+
+        if(empty($errors)){
+            $query = "INSERT INTO game(gameName, gameDesciption, gamePrice, categoryID) VALUES ('$gameName','$gameDesc','$gamePrice','$gameCategory')";
+            $result = mysqli_query($conn,$query);
+            echo '<script> alert("Game added successfully");
+            window.location.href="admin.php"; </script>';
+            
+        }   
     }
 ?>
 
-<h3 class="text-center mb-5">ADD GAMES</h3>
 
-<form action="" method="POST" class="mb-2" enctype="multipart/form-data">
+<form action="admin.php?addgames" method="POST" class="mb-2" enctype="multipart/form-data">
 
-    
+    <h3>ADD GAMES</h3>
 
-    <div class="form-outline w-50 mb-4 mt-4 m-auto">
-            <label for="game_title" class="form-label">Game Title</label>
-            <input type="text" id="game_title" class="form-control" required="required  " placeholder="Enter Game Title" name="add-game" aria-label="gamename" aria-describedby="addon-wrapping" autocomplete="off">
-    </div>
+    <div class="input w-90 mb-2">
+        <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping"><i class="fa-solid fa-reciept"></i>
+            </span>
+            <input type="text" class="form-control" placeholder="Name" name="add-game" aria-label="Username"
+                aria-describedby="addon-wrapping">
+        </div>
 
-    <div class="form-outline w-50 mb-4 mt-4 m-auto">
-            <label for="game_desc" class="form-label">Game Description</label>
-            <input type="text" class="form-control" placeholder="Enter Game Description" name="add-desc" aria-label="gamedesc" aria-describedby="addon-wrapping">
-    </div>
+        <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping"><i class="fa-solid fa-reciept"></i>
+            </span>
+            <textarea class="form-control" placeholder="Desciption" name="add-desc" aria-label="Username"
+                aria-describedby="addon-wrapping" col="30" rows="5"></textarea>
+        </div>
 
-    <div class="form-outline w-50 mb-4 mt-4 m-auto">
-            <label for="game_keyword" class="form-label">Game Keyword</label>
-            <input type="text" class="form-control" placeholder="Enter Game Keyword" name="add-keyword" aria-label="gamekey" aria-describedby="addon-wrapping">
-    </div>
+        <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping"><i class="fa-solid fa-reciept"></i>
+            </span>
+            <input type="text" class="form-control" placeholder="Price" name="add-price" aria-label="Username"
+                aria-describedby="addon-wrapping">
+        </div>
 
-    <div class="form-outline w-50 mb-4 mt-4 m-auto">
-            <label for="game_genre" class="form-label">Game Genre</label>
-            <select name="add-genre" id="game_genre" class="form-select">
-                <option value="">Select Genre</option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+        <!-- <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping"><i class="fa-solid fa-reciept"></i>
+            </span>
+            <input type="file" class="form-control" placeholder="Image" name="add-picture" aria-label="Username"
+                aria-describedby="addon-wrapping">
+        </div> -->
+
+        <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping"><i class="fa-solid fa-reciept"></i>
+            </span>
+            <select class="form-control" placeholder="Name" name="add-category" aria-label="Username"
+                aria-describedby="addon-wrapping">
+                <option value=""> choose category </option>
+                <?php
+                require_once('connection.php');
+                $query = "SELECT * FROM category";
+                $result = mysqli_query($conn,$query);
+                while($row = mysqli_fetch_assoc($result)){
+                    echo '<option value='.$row['categoryID'].'>'.$row['categoryName'].'</option>';
+            }
+            ?>
             </select>
+
+        </div>
     </div>
 
-    <div class="form-outline w-50 mb-4 mt-4 m-auto">
-            <label for="game_image" class="form-label">Upload Image</label>
-            <input type="file" class="form-control" name="add-img" aria-label="gamedesc" aria-describedby="addon-wrapping">
-    </div>
-
-    <div class="form-outline w-50 mb-4 mt-4 m-auto">
-            <label for="game_price" class="form-label">Game Price</label>
-            <input type="text" class="form-control" placeholder="Enter Game Price" name="add-price" aria-label="gamedesc" aria-describedby="addon-wrapping">
-    </div>
-
-
-    <div class="form-outline w-50 mb-4 mt-4 m-auto">
-         <div class="input-group flex-nowrap">
-            <input type="submit" class="btn btn-dark px-3 my-3 border-0" value="Submit" name="submit-game" 
-            aria-label="Username" aria-describedby="addon-wrapping"> 
+    <div class="input w-90 mb-2">
+        <div class="input-group flex-nowrap">
+            <input type="submit" class="btn btn-dark p-2 my-3 border-0" value="Submit" name="submit-game"
+                aria-label="Username" aria-describedby="addon-wrapping">
         </div>
     </div>
 </form>
